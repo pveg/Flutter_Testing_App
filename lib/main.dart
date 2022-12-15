aebc3454914878f1076fb673ed4974e3388b1915
Paulo Torres <ctw02561@Victors-MBP.home>
2022-12-15 20:15:35 +0000

    onPressed function added

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyTestingApp());
 
 class MyTestingApp extends StatelessWidget {
+  void answerQuestion() => print('answer chosen!');
+
   @override //makes clear that we overrides the build method
   Widget build(BuildContext context) {
     //Needs Type and Context
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My first app')), //appbar
-        body: Text('This is the default body'),
+        body: Column(children: <Widget>[
+          Text('Questions'),
+          ElevatedButton(
+              onPressed: answerQuestion, child: Text('Answer 1')),
+          ElevatedButton(
+              onPressed: answerQuestion, child: Text('Answer 2')),
+          ElevatedButton(
+              onPressed: answerQuestion, child: Text('Answer 3')),
+        ]),
       ),
     ); //receive the named arguments
   }
