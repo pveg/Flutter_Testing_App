6f0a3e9df2aebcafb67eba895755ef58751f4f20
Paulo Torres <ctw02561@Victors-MBP.home>
2022-12-15 21:28:53 +0000

    state management

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,39 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyTestingApp());
 
-class MyTestingApp extends StatelessWidget {
-  void answerQuestion() => print('answer chosen!');
+class MyTestingApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+
+    return MyTestingAppState();
+  }
+}
+
+class MyTestingAppState extends State<MyTestingApp> {
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+    questionIndex++;
+    });
+    print(questionIndex);
+  }
 
   @override //makes clear that we overrides the build method
   Widget build(BuildContext context) {
+    var questions = [
+      'what is your favorite color',
+      'what is your favorite animal'
+    ];
     //Needs Type and Context
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My first app')), //appbar
         body: Column(children: <Widget>[
-          Text('Questions'),
-          ElevatedButton(
-              onPressed: answerQuestion, child: Text('Answer 1')),
-          ElevatedButton(
-              onPressed: answerQuestion, child: Text('Answer 2')),
-          ElevatedButton(
-              onPressed: answerQuestion, child: Text('Answer 3')),
+          Text(questions[questionIndex]),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 2')),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3')),
         ]),
       ),
     ); //receive the named arguments
